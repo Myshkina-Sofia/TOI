285f0422760647140c78c5c7c1e0505663a9c39f
Myshkina-Sofia <152999033+Myshkina-Sofia@users.noreply.github.com>
2024-01-27 14:14:26 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,19 @@
-﻿namespace TOIlab2
+﻿namespace lab2_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите значение M:");
-            int m = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите значение N:");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            int akkerman(int m, int n)
+            int makkarty(int n)
             {
-                if (m == 0) return n + 1;
-                else if (n == 0) return akkerman(m -1, 1);
-                else return akkerman(m -1, akkerman(m, n -1));
+                if (n > 100) return n - 10;
+                else return makkarty(makkarty(n + 11));
             }
 
-            Console.Write($"Функция Аккермана равно { akkerman(m, n)} ");
+            Console.Write($"Функция Аккермана равно {makkarty(n)} ");
         }
     }
 }
